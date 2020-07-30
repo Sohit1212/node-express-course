3f400cab7af105aef5c10050aeb8abc8037682fe
Sohit1212 <sohit.gore21@outlook.com>
2020-07-30 13:05:10 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
 
 const mockUserData = [
 	{name:'Mark'},
 	{name:'Jill'}
 ]
 
-app.get('/users',function(req,res){
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id);
 	res.json({
 		success: true,
 		message: 'successfully got users. Nice!',
 		users: mockUserData
 	})
+})
+
+app.post('/login', function(req,res){
+	const username = req.body.username;
+	const password = req.body.password;
+
+	const mockuname = 'Sohit';
+	const mockpaswd = '1212';
+
+	if(username === mockuname && password === mockpaswd) {
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
+	} else {
+		res.json({
+			success: false,
+			message: 'password or username does not match'
+		})
+	}
 })
 
 
